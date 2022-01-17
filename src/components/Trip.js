4413cf2c1bb4750556c43072caf9d2273ed6d206
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-17 18:34:13 +0100

    fetched data

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -7,25 +7,25 @@ export default function Trip() {
   const [trips, setTrips] = useState([])
   const [url, setUrl] = useState('http://localhost:8000/trips')
 
-  useEffect((url) => {
-    fetch()
+  useEffect(() => {
+    fetch(url)
       .then(res => res.json())
       .then(json => setTrips(json))
   }, [url])
 
-  
+  console.log(trips)
 
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map(trip => {
+        {trips.map(trip => (
           <li key={trip.id}>
             <h3>{trip.title}</h3>
             <p>{trip.price}</p>
           </li>
-        })}
+        ))}
       </ul>
 
     </div>
