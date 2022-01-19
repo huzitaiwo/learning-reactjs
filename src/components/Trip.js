0e8b98ee9b8aa0ab17cd746e673f1d3d0e85aa7a
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-19 12:05:27 +0100

    fecthed trips resource

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,6 +1,10 @@
-import React from 'react';
+
 
 export default function Trip() {
+
+  fetch('http://localhost:8000/trips')
+    .then(res => res.json())
+    .then(json => console.log(json))
   return (
     <div>Trip</div>
   )
