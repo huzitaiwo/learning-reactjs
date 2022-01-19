edf7b4e9706d80cd90468fc9ba87cbb3087bf61e
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-19 12:09:55 +0100

    setup trips state

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,11 +1,17 @@
+import { useState } from "react"
 
 
 export default function Trip() {
+  const [trips, setTrips] = useState([])
+
+
 
   fetch('http://localhost:8000/trips')
     .then(res => res.json())
     .then(json => console.log(json))
+
+
   return (
-    <div>Trip</div>
+    <h1>Trip List</h1>
   )
 }
