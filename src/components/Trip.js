3a17bd6b63f597b6873c6b9fff6703efec938571
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-19 12:20:16 +0100

    edited db.json file

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react"
 export default function Trip() {
   const [trips, setTrips] = useState([])
 
-  useEffect()
+  useEffect(() => {
+    fetch('http://localhost:8000/trips')
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }, [])
 
-  fetch('http://localhost:8000/trips')
-    .then(res => res.json())
-    .then(json => console.log(json))
+  
 
 
   return (
