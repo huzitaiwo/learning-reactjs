da22800dc2091c1f2220bc30114762e98e77aca6
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-19 03:55:59 +0100

    created fetchData func inside useEffect func

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,13 @@
+import { useEffect, useState} from 'react'
+
+export const useFetch = url => {
+  const [data, setData] = useState()
+//   const [isPending, setIsPending] = useState()
+//   const [error, setError] = useState()
+
+  useFetch(() => {
+    const fetchData = async () => {
+        
+    }
+  }, [url])
+}
