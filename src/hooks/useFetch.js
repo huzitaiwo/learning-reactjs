ecd4629104f179864d186c4d9b8e7945c8e5488d
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-20 03:58:01 +0100

    created isPending and error state

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,12 +2,15 @@ import { useEffect, useState} from 'react'
 
 export const useFetch = url => {
   const [data, setData] = useState()
-//   const [isPending, setIsPending] = useState()
-//   const [error, setError] = useState()
+  const [isPending, setIsPending] = useState()
+  const [error, setError] = useState()
 
   useFetch(() => {
     const fetchData = async () => {
-        
+      const res = await fetch(url)
+      const json = await res.json()
+
+      setData(json)
     }
   }, [url])
 }
