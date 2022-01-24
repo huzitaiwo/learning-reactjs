58e9389d6e8b534da7bd76452cf47ff6057e106b
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-24 22:23:36 +0100

    created Modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// components
+import Modal from './component/Modal'
+
 // styles
 import './App.css'
+
 
 export default function App() {
   return (
     <div className="app">
         <h1>App Component</h1>
+        <Modal />
     </div>
   )
 }
