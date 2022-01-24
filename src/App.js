b38dcc60282b0aed75625c6d2d9ace43ac842068
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-24 23:08:31 +0100

    toggled showModal

--- src/App.js
+++ src/App.js
@@ -9,25 +9,24 @@ import './App.css'
 
 
 export default function App() {
-  const [showModal, setShowModal] = useState(true)
+  const [showModal, setShowModal] = useState(false)
 
   const handleClose = () => {
     setShowModal(false)
   }
 
-  console.log(showModal)
 
   return (
     <div className="app">
       <h1 className="title">App Component</h1>
-      {/* <Modal>
-        <h2>10% Off Coupon Code</h2>
-        <p>Use the code NINJA10 at the checkout.</p>
-      </Modal> */}
-      <Modal handleClose={handleClose}>
+      
+      {showModal && <Modal handleClose={handleClose}>
         <h2>Terms and Conditions</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veniam quisquam doloribus beatae minima. Voluptatem atque ipsam accusantium excepturi, vel laudantium tempore temporibus unde molestiae natus.</p>
-      </Modal>
+      </Modal>}
+      <div>
+        <button onClick={() => setShowModal(true)}>show modal</button>
+      </div>
     </div>
   )
 }
