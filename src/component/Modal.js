91bc6b1b841e49be6e7f8a42382f9385509cb544
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-24 22:41:47 +0100

    created Modal component

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -2,7 +2,8 @@
 export default function Modal() {
   return (
     <div className="modal-backdrop">
-        <h1>Modal</h1>
+      <h2>10% Off Coupon Code</h2>
+      <p>Use the code NINJA10 at the checkout.</p>
     </div>
   )
 }
