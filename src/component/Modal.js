333c3051951aa07bf17ddd907c35bd350210846e
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-25 14:16:44 +0100

    styled Modal component with inline styles

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -6,7 +6,11 @@ import './Modal.css'
 export default function Modal({ children, handleClose }) {
   return ReactDom.createPortal((
     <div className="modal-backdrop">
-      <div className="modal">
+      <div className="modal" style={{
+        border: "1px solid",
+        borderColor: "#141414",
+        textAlign: "left"
+        }}>
         {children}
         <button onClick={handleClose}>close</button>
       </div>
