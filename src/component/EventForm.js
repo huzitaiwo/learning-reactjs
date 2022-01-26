380eb55c270424e4e911df4882a528c1acc091a7
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-26 14:42:15 +0100

    imported EventForm.css to EventForm.js file

---
+++ src/component/EventForm.js
@@ -0,0 +1,12 @@
+import './EventForm.css'
+
+export default function EventForm() {
+  return (
+    <form>
+      <label>
+        <span>Event Title</span>
+        <input type="text" />
+      </label>
+    </form> 
+  )
+}
