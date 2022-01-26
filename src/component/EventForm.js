be8f74d2a75982df871db885bfe30d9669f56afb
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-26 14:58:42 +0100

    implemented title and date state

--- src/component/EventForm.js
+++ src/component/EventForm.js
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import './EventForm.css'
 
 export default function EventForm() {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+
+
   return (
     <form className="event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" />
+        <input 
+          type="text" 
+          onChange={e => setTitle(e.target.value)}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" />
+        <input 
+          type="date" 
+          onChange={e => setDate(e.target.value)} />
       </label>
       <button>Submit</button>
     </form> 
