49434da86ebb9d11053d2d52ab602125fe643181
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-27 17:08:02 +0100

    hooked up the shuffleCards function

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
