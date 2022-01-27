2aa4afae317b0090f8795bc227da90e28dbca891
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-27 17:23:12 +0100

    created Card compoenent

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+
+
+export default function Card({ card }) {
+  return (
+    <div className='card' key={card.id}>
+      <div>
+        <img className='front' src={card.src} alt='card front' />
+        <img className='back' src='img/cover.png' alt='card back' />
+      </div>
+    </div>
+  )
+}
