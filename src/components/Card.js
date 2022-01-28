405b631c0dd04a0a5a5fd6a5920215860bec78fb
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-28 18:51:20 +0100

    created handleClick function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,19 @@
 import './Card.css'
 
 export default function Card({ card }) {
+  const handleClick = () => {
+
+  }
+
   return (
     <div className='card'>
       <div>
         <img className='front' src={card.src} alt='card front' />
-        <img onClick={handleClick} className='back' src='img/cover.png' alt='card back' />
+        <img 
+          onClick={handleClick} 
+          className='back' 
+          src='img/cover.png' 
+          alt='card back' />
       </div>
     </div>
   )
