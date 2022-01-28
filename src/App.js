eaf5ede7b2d67054e875b317565ea7cea569dc0f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-28 15:20:54 +0100

    added matched keys to initial cards

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Card from './components/Card';
 import "./App.css";
 
 const cardImages = [
-  {"src": "img/helmet-1.png"},
-  {"src": "img/potion-1.png"},
-  {"src": "img/ring-1.png"},
-  {"src": "img/scroll-1.png"},
-  {"src": "img/shield-1.png"},
-  {"src": "img/sword-1.png"}
+  {"src": "img/helmet-1.png", matched: false},
+  {"src": "img/potion-1.png", matched: false},
+  {"src": "img/ring-1.png", matched: false},
+  {"src": "img/scroll-1.png", matched: false},
+  {"src": "img/shield-1.png", matched: false},
+  {"src": "img/sword-1.png", matched: false}
 ]
 
 
