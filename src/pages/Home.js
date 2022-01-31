49ffff8417bf7e7fc60e650f9f8934a71b737f08
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-31 03:59:16 +0100

    Created Home.js file

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cumque provident. Vero vitae molestias, blanditiis, labore maxime esse dolore sunt officiis facilis sit unde dolor expedita voluptatem sapiente laboriosam quo.</p>
+    </div>
+  )
+}
