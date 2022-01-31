b074c33d3f4bdd75f60d27ad002150d8490b69fb
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-31 17:28:33 +0100

    fixed redirect error

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -6,6 +6,8 @@ export default function Article() {
   const { id } = useParams()
   const url = 'http://localhost:4000/articles/' + id
   const {data: article, isPending, error } = useFetch(url)
+  const history = useHistory()
+
 
   useEffect(() => {
     if(error) {
