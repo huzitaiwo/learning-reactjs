0931fbc69e2af52a5293e78e8c093f9e4f3b3ce9
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-31 04:00:06 +0100

    Created About.js file

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cumque provident. Vero vitae molestias, blanditiis, labore maxime esse dolore sunt officiis facilis sit unde dolor expedita voluptatem sapiente laboriosam quo.</p>
+    </div>
+  )
+}
