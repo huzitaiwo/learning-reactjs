a8cd061a0857fff63fe6ad94e22c6d617727d8ad
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-01 17:42:14 +0100

    used javascript URLSearchParams

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,11 +2,13 @@ import { useLocation } from "react-router-dom"
 
 export default function Contact() {
   const queryString = useLocation().search
-  console.log(queryString)
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
 
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>HI {name}, Contact us here</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cumque provident. Vero vitae molestias, blanditiis, labore maxime esse dolore sunt officiis facilis sit unde dolor expedita voluptatem sapiente laboriosam quo.</p>
     </div>
   )
