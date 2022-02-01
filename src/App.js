11f92a83aa56241c7acb6c6a4a39685271e248b0
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-01 17:33:20 +0100

    used Route Redirect package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom'
 
 // styles
 import "./App.css";
@@ -33,6 +33,9 @@ function App() {
           </Route>
           <Route path="/articles/:id">
             <Article />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </BrowserRouter>
