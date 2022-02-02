65348cb9da90d8599e2d353364538f9dff34035c
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-02 17:49:18 +0100

    cleaned up App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom'
-
 // styles
 import "./App.css";
-
-// page components
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Article from './pages/Article'
 
 function App() {
   return (
