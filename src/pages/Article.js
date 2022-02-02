334a07da2b2a73a496da344e1e16360d5e6eae0f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-02 04:44:10 +0100

    extracted useParams id

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export default function Article() {
+  const { id } = useParams()
+
+
   return (
     <div>
-      <h2>Article page</h2>
+      <h2>Article page - {id}</h2>
     </div>
   )
 }
