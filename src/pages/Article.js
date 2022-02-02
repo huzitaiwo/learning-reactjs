085811f31b9a5b13dcb5063a968a4cab0a4110ab
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-02 04:47:08 +0100

    impoted useFetch hook

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 
 export default function Article() {
   const { id } = useParams()
+  const url = 'http://localhost:4000/articles/' + id
+  const {data: article, isPending, error } = useFetch(url)
 
 
   return (
