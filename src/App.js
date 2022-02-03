4e1668dbbcf64516bfababe3001b0f999d5280ac
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-03 20:45:53 +0100

    setup Routes for 4 pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import { BrowserRouter, Switch, Route  } from "react-router-dom";
 // styles
 import "./App.css";
 
+// pages and components
+import Home from './pages/home/Home'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+import Create from './pages/create/Create'
+
 function App() {
   return (
     <div className="App">
-      App component
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
