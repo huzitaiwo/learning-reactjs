f81fcd9b6cba6befc96698388b1129efa8bc4ee6
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-03 21:01:07 +0100

    used useFetch hook

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,12 @@
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      
+    </div>
   )
 }
