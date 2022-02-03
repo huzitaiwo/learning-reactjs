798330fb698ad0312781edf70ab2ad6141cf77c8
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-03 21:05:01 +0100

    used data from useFetch hook

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,7 +8,11 @@ export default function Home() {
 
   return (
     <div>
-      
+      {error && <h3 className='error'>{error}</h3>}
+      {isPending && <h3>Loading...</h3>}
+      {recipes && recipes.map(recipe => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
     </div>
   )
 }
