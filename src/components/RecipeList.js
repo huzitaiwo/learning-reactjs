c634ea773bbc2f133b807d6639ab062b58dcd967
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-03 21:23:00 +0100

    styled RecipeList component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -10,7 +10,7 @@ export default function RecipeList({ recipes }) {
         <div key={recipe.id} className='card'>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
-          <div>{recipe.method.subString(0, 100)}...</div>
+          <div>{recipe.method.substring((0, 100))}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
         </div>
       ))}
