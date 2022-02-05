302fde9db36993337707626d281a3b319236db48
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-05 13:17:32 +0100

    created title input

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -4,7 +4,13 @@ import './Create.css'
 export default function Create() {
   return (
     <form>
-
+      <label>
+        <span>title:</span>
+        <input 
+        type="text"
+         
+      />
+      </label>
     </form>
   )
 }
