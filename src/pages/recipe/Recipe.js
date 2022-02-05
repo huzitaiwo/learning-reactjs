579e18e464a7876d453d86e298bd4a1109e9fcac
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-05 13:14:26 +0100

    completed Loading individual recipes

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -6,13 +6,22 @@ import './Recipe.css'
 
 export default function Recipe() {
   const { id } = useParams()
-  const url = 'http://localhost:4000/recipes' + id
+  const url = 'http://localhost:4000/recipes/' + id
 
   const { data:recipe, isPending, error } = useFetch(url)
 
   return (
     <div>
-      Recipe - {id}
+      {error && <h3>{error}</h3>}
+      {isPending && <h3>Loading...</h3>}
+      {recipe && (
+        <div className='card'>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method}</div>
+          <p>{recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}</p>
+        </div>
+      )}
     </div>
   )
 }
