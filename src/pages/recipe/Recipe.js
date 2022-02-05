dddbfab862bbe9a0b22e1fbc59ccf38fff42985e
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-05 13:03:15 +0100

    used useFetch hook in Recipe Component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 
 // styles
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:4000/recipes' + id
+
+  const { data:recipe, isPending, error } = useFetch(url)
+
   return (
-    <div>Recipe</div>
+    <div>
+      Recipe - {id}
+    </div>
   )
 }
