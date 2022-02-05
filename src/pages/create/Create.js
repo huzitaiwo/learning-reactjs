8ad026d6fef5771ab72dcde25c1f701d91d4f472
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-05 03:35:57 +0100

    created states for recipe Form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css'
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+  const [ingredients, setIngredients] = useState('')
+
   return (
     <form>
       <label>
         <span>title:</span>
         <input 
         type="text"
-         
+
       />
       </label>
     </form>
