1a88bb78e3d5c6e00812894291f6c7d05ff1f1de
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-12 03:58:37 +0100

    created ingredients states

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -7,6 +7,8 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState('')
 
   const handleSubmit = e => {
     e.preventDefault()
