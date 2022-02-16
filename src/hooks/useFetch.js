840384fcb02faaac561c0cc59e0dbad975d767a7
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-16 03:40:57 +0100

    created postData function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,6 +6,16 @@ export const useFetch = (url, method = "GET") => {
   const [error, setError] = useState(null)
   const [options, setOptions] = useState(null)
 
+  const postData = postData => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+  }
+
   useEffect(() => {
     const controller = new AbortController()
 
