bf908eedba4373108f11df08cbb53395f5c313cd
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-17 04:11:13 +0100

    created handleSubmit function

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 // styles
 import './Searchbar.css'
@@ -6,15 +7,22 @@ import './Searchbar.css'
 
 export default function Searchbar() {
   const [term, setTerm] = useState('')
-  
+  const history = useHistory()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    history.push(`/search?q=${term}`)
+  }
+ 
   return (
     <div className='searchbar'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="search">Search:</label>
         <input 
           type="text" 
           id='search' 
-          onChange={} 
+          onChange={e => setTerm(e.target.value)}
+          required
         />
       </form>
     </div>
