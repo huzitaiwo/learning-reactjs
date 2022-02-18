5b8686de3ab75da4185feb8ce51c2b8dba63dd05
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-18 23:05:03 +0100

    styled Recipe component

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -2,7 +2,7 @@ import { createContext } from 'react'
 
 export const ThemeContext = createContext()
 
-export function ThemePrivider({ children }) {
+export function ThemeProvider({ children }) {
 
   return (
     <ThemeContext.Provider value={{ color: 'blue'}}>
