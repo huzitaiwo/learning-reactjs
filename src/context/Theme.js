62c2f3b24ab89adff8f60be152fdeca25c0c7b4f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-18 22:44:44 +0100

    created ThemeContext.js

--- src/context/Theme.js
+++ src/context/Theme.js
@@ -1,1 +1,12 @@
 import { createContext } from 'react'
+
+export const ThemeContext = createContext()
+
+export function ThemePrivider({ children }) {
+
+  return (
+    <ThemeContext.Provider value={{ color: 'blue'}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
