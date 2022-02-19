9d4589d0219ab739c422003daf59b291e21a49ba
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-19 05:00:30 +0100

    retrieved profiles

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -5,9 +5,22 @@ import './Profile.css'
 
 
 export default function Profile() {
-  const { data: pofiles, error, isPending } = useFetch
+  const { data, error, isPending } = useFetch('https://api.github.com/search/users?q=lagos&page=1')
+
+  const profiles = data.items
+  console.log(profiles)
 
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      {error && <h2>{error}</h2>}
+      {isPending && <h2>Loading...</h2>}
+      {profiles && profiles.map(profile => (
+        <div key={profile.id} className='card'>
+          {/* <img src={profile.avatar_url} alt="" /> */}
+          <p>{profile.login}</p>
+          <p>{profile.score}</p>
+        </div>
+      ))}
+    </div>
   )
 }
