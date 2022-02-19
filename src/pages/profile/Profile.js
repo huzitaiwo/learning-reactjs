2799e8940af9d72f14251349b4734be1d22ca523
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-19 05:16:39 +0100

    fixing layout

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -7,14 +7,15 @@ import './Profile.css'
 export default function Profile() {
   const { data, error, isPending } = useFetch('https://api.github.com/search/users?q=lagos&page=1')
 
-  const profiles = data.items
-  console.log(profiles)
+//   const profiles = data.items
+  console.log(data.items)
+  
 
   return (
     <div className='profile'>
       {error && <h2>{error}</h2>}
       {isPending && <h2>Loading...</h2>}
-      {profiles && profiles.map(profile => (
+      {data.items && data.items.map(profile => (
         <div key={profile.id} className='card'>
           <img src={profile.avatar_url} alt="" />
           <p>{profile.login}</p>
