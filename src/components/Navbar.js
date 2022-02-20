fbf5016031bba3114f5133b7af7911047b4f9b30
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-20 23:12:47 +0100

    used ThemeContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+
 // styles
 import './Navbar.css'
 
 // components
 import Searchbar from './Searchbar'
 
-
 export default function Navbar() {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
       <nav>
         <Link className='brand' to="/">
           <h1>Cooking Ninja</h1>
