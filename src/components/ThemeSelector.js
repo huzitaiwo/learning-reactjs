60efe95e427d902f84ed47dd6752612abfd400cb
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-17 01:26:36 +0100

    created ThemeSelector component

---
+++ src/components/ThemeSelector.js
@@ -0,0 +1,20 @@
+import { useTheme } from '../hooks/useTheme'
+
+// styles
+import './ThemeSelector.css'
+
+const themeColors = ['#58249c', '#249c6b', '#b70233']
+
+export default function ThemeSelector() {
+  const { changeColor } = useTheme()
+
+  return (
+    <div className='theme-selector'>
+      <div className="theme-buttons">
+        {themeColors.map(color => (
+          <div key={color} onClick={() => changeColor(color)}/>
+        ))}
+      </div>
+    </div>
+  )
+}
